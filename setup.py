40699b8afb3af8f77b72a101c91ccba76587b7c9
Andrew Jones <andyjones.ed@gmail.com>
2019-09-26 22:03:21 +0100

    Updates the version num

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coolgpus',
-    version='0.1',
+    version='0.11',
     description='GPU fan control for (partially?) headless Linux',
     url='https://github.com/andyljones/coolgpus',
     author='Andy L Jones',
