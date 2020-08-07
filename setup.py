86d1c415383ca37ac485220585ec481450af86a2
Andrew Jones <andyjones.ed@gmail.com>
2020-08-07 13:57:36 +0100

    Fixes timeout

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coolgpus',
-    version='0.21',
+    version='0.22',
     description='GPU fan control for headless Linux',
     url='https://github.com/andyljones/coolgpus',
     author='Andy L Jones',
