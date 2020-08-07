b750daf84cc5e287d69487164af441c1d23584ef
Andrew Jones <andyjones.ed@gmail.com>
2020-08-07 13:27:45 +0100

    Adds verbose switch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coolgpus',
-    version='0.19',
+    version='0.20',
     description='GPU fan control for headless Linux',
     url='https://github.com/andyljones/coolgpus',
     author='Andy L Jones',
